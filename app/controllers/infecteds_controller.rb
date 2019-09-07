d97bfc4b39ff19a8877180bbea4b9a9362b42989
moises <moisessousa241@gmail.com>
2019-09-07 17:18:50 -0400

    infecteds create method counting the survivor_id to mark it as infected

--- app/controllers/infecteds_controller.rb
+++ app/controllers/infecteds_controller.rb
@@ -1,20 +1,27 @@
 class InfectedsController < ApplicationController
 
   def index
-  	infecteds = Infected.all
-  	render json:{staus: "ok", message:"success", data: infecteds}
+    infecteds = Infected.all
+    render json:{staus: "ok", message:"success", data: infecteds}
   end
 
   def create
-  	infected = Infected.new(infected_params)
-  	if infected.save
-  		render json:{status: "ok", message: "success", data: infected}, status: :ok
-  	else
-  		render json:{status: "error", message: "failed", data: infected.errors}, status: :error
-  	end
+    survivor_id = params[:survivor_id]
+
+    if(Infected.count(survivor_id) >= 3)
+      Survivor.update(survivor_id, :infected => 1)
+      render json:{status: "ok", message: "infected"}, status: :ok
+    elsif
+      infected = Infected.new(infected_params)
+      if infected.save
+        render json:{status: "ok", message: "success", data: infected}, status: :ok
+      end
+    else
+      render json:{status: "error", message: "failed", data: infected.errors}, status: :error
+    end
   end
 
   def infected_params
-  	params.permit(:survivor_id)
+    params.permit(:survivor_id)
   end
 end
