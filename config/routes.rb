dd89c4dcf416026238fc64b1485afac141c201ec
moises <moisessousa241@gmail.com>
2019-09-07 22:58:12 -0400

    route error fixed

--- config/routes.rb
+++ config/routes.rb
@@ -4,19 +4,20 @@ Rails.application.routes.draw do
   get 'infecteds/show'
   get 'infecteds/create'
   
-  resources :items
   get 'items/show'
   get 'items/update'
   get 'items/create'
   get 'items/destroy'
  
-  resources :survivors
   get 'survivors/show'
   get 'survivors/index'
   get 'survivors/destroy'
   get 'survivors/update'
   get 'survivors/create'
-
   get 'survivors/location/:id/:latitude/:longitude' => 'survivors#location'
+  
+  resources :items
+  resources :survivors
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
