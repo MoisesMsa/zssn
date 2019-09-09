ba75ef3c83cff4deec8895ad9731362534194bff
moises <moisessousa241@gmail.com>
2019-09-09 14:50:24 -0400

    calc items points to validate trade

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -8,21 +8,33 @@ class InventoriesController < ApplicationController
 		if(s1.infected || s2.infected) 
 			render json: {error: "infected"}, status: :error
 		else
-			# if check_points([survivor_1, survivor_2])
-				update(params[:id1], params[:items][0], params[:items][1])
-				update(params[:id2], params[:items][1], params[:items][0])
-			# end
+			#inventory in database
+			s1_inventory = inventory_items(params[:id1])
+			s2_inventory = inventory_items(params[:id2])
+
+			#url items to trade
+			items_s1 = params[:items][:survivor1]
+			items_s2 = params[:items][:survivor2]
+
+			#validate if the trade values are equals
+			if(calc_points(s1_inventory, items_s1) == calc_points(s2_inventory, items_s2))
+				update(s1_inventory, items_s1, items_s2)
+				update(s2_inventory, items_s2, items_s1)
+			end
 		end
 	end
 
 	private 
-		def update(survivor_id, items_remove, items_add)
-			
-			items = Item.select('inventories.survivor_id', 'items.name', 'inventories.total')
+
+		def inventory_items(survivor_id)
+			items = Item.select('inventories.survivor_id', 'items.name', 'inventories.total', 'items.points')
 			  		.joins(:inventory)
 			  		.where('inventories.survivor_id' => survivor_id)
-			
-			items.each do |item|
+		end
+
+		def update(inventory_items, items_remove, items_add)
+
+			inventory_items.each do |item|
 				for i in 1..0
 
 					item_name = item.name.downcase
@@ -34,10 +46,20 @@ class InventoriesController < ApplicationController
 						total = inventory.total + total
 	                end
 
-	                total = total <= 0 ? 0 : total
+	                total = (total <= 0) ? 0 : total
 	                
 	                inventory.update(:total => total)
 				end
-            end
+	        end
+		end
+
+		def calc_points(inventory_items, items_totals)
+			points = 0
+			inventory_items.each do |item|
+				name = item.name.downcase
+				if items_totals.key?(name)
+			  		points += items_totals[name].to_i*item.points
+			  	end
+			end
 		end
 end
