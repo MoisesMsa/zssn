d97caecb26ab995fb73821238e50b0336dd9eef9
moises <moisessousa241@gmail.com>
2019-09-09 13:49:07 -0400

    trade items update items total

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -1,56 +1,43 @@
 class InventoriesController < ApplicationController
 
 	def trade
-		#get all the records in the database where the paramater matchs
 
-		# survivor_1 = Survivor.survivor_inventory(params[:id1], params[:trade][0][:sur_id])
-		# survivor_2 = Survivor.survivor_inventory(params[:id2], params[:trade][1][:sur_id])
+		s1 = Survivor.select(:infected).find(params[:id1])
+		s2 = Survivor.select(:infected).find(params[:id2])
 		
-		data = Inventory.select(:id,'items.name', 'inventories.total')
-			  .joins(:item)
-			  .group('inventories.survivor_id', 'items.name')
-
-		render json: {survivor_1: data }
-
-		# if(params[:id1].nil? || params[:id2].nil? || params[:items1].nil? || params[:items2].nil?)
-		# 	render json: {error: "empty values"}, status: :error
-		
-		# if(survivor_1[0][:infected] || survivor_2[0][:infected]) 
-		# 	render json: {error: "infected"}, status: :error
-		# else
-		# 	totals
-		# 	if check_points([survivor_1, survivor_2])
-		# 		# update(survivor_1)
-		# 	end
-		# end
-	end
-
-	def totals
-		t = params[:items1]
-		t.inject(Hash.new(0)) { |total, e| total[e] += 1; total}
-		puts t.inspect
-	end
-
-	def check_points(items)	
-		points = Array.new
-		
-		items.each_with_index do |item, index|
-		  sum = item.sum{|i| i.total*i.points}
-		  points.push sum
-		end		
-
-		points[0] == points[1] ? true : false
+		if(s1.infected || s2.infected) 
+			render json: {error: "infected"}, status: :error
+		else
+			# if check_points([survivor_1, survivor_2])
+				update(params[:id1], params[:items][0], params[:items][1])
+				update(params[:id2], params[:items][1], params[:items][0])
+			# end
+		end
 	end
 
 	private 
+		def update(survivor_id, items_remove, items_add)
+			
+			items = Item.select('inventories.survivor_id', 'items.name', 'inventories.total')
+			  		.joins(:inventory)
+			  		.where('inventories.survivor_id' => survivor_id)
+			
+			items.each do |item|
+				for i in 1..0
 
-		def update(items_names, items_totais)
-			for i in 0..items.size
-				if Survivor.joins(:item).where('item.name' => items_names[i]).exists?
-					items.update_attributes(:total => items_totais[i])
-				else	
-					items = create()
+					item_name = item.name.downcase
+	                total = items_remove[item_name]
+
+	                if (items_remove.key?(item_name)) 
+						total = inventory.total - total
+					elsif items_add.key?(item_name)
+						total = inventory.total + total
+	                end
+
+	                total = total <= 0 ? 0 : total
+	                
+	                inventory.update(:total => total)
 				end
-			end
+            end
 		end
 end
